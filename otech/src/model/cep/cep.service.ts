da13115b759bea227dbfb09923f87a062412a376
adaleixo <isaac.aleixo@gmail.com>
2020-10-18 16:54:15 -0300

    Criadas as classes para consumir a API de filmes, criados os atributos para o filme.

--- otech/src/model/cep/cep.service.ts
+++ otech/src/model/cep/cep.service.ts
@@ -1,23 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { BehaviorSubject, Observable, throwError } from "rxjs";
 import { map, retry, catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
-
+  providedIn: "root",
 })
 export class CepService {
-
-  recuperarEnderecoPorCep(cep){
-      return this.http.get<any>(`https://viacep.com.br/ws/${cep}/json/` ).pipe(
-        map((result) => {  
-          return result;
-        }),
-      );
+  recuperarEnderecoPorCep(cep) {
+    return this.http.get<any>(`https://viacep.com.br/ws/${cep}/json/`).pipe(
+      map((result) => {
+        return result;
+      })
+    );
   }
-  
 
-  constructor(private http:HttpClient) {} 
-
+  constructor(private http: HttpClient) {}
 }
---
+++ otech/src/model/movie/movie.ts
@@ -0,0 +1,8 @@
+export class Movie {
+  poster_path: string;
+  id: number;
+  title: string;
+  vote_average: number;
+  overview: string;
+  release_date: string;
+}
